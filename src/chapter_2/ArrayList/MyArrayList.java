337bb730171829cbb7d78b2ad51ecccea12bed42
Cop-Tur <824919441@qq.com>
2020-03-15 23:50:13 +0800

    ArrayList泛型类实现

--- src/chapter_2/ArrayList/MyArrayList.java
+++ src/chapter_2/ArrayList/MyArrayList.java
@@ -76,7 +76,17 @@ public class MyArrayList <AnyType> implements Iterable<AnyType> {
     }
 
     private class ArrayListIterator implements java.util.Iterator<AnyType> {
-
-
+        private int current = 0;
+        public boolean hasNext(){
+            return current<size();
+        }
+        public AnyType next(){
+            if (!hasNext())
+                throw new java.util.NoSuchElementException();
+            return theItems[current++];
+        }
+        public void remove(){
+            MyArrayList.this.remove(--current);
+        }
     }
 }
