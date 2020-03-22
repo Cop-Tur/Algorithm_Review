5f8718dd8a3e5bc233e67263e7d5cc79c49041de
Cop-Tur <824919441@qq.com>
2020-03-22 13:46:26 +0800

    chapter_3 tree

--- src/chapter_3/binarytree/BinaryNode.java
+++ src/chapter_3/binarytree/BinaryNode.java
@@ -4,4 +4,13 @@ public class BinaryNode<AnyType> {
     BinaryNode(AnyType theElement){
         this(theElement,null,null);
     }
+    BinaryNode(AnyType theElement,BinaryNode<AnyType> lt,BinaryNode<AnyType> rt){
+        element=theElement;
+        left=lt;
+        right=rt;
+    }
+
+    AnyType element;
+    BinaryNode<AnyType> left;
+    BinaryNode<AnyType> right;
 }
