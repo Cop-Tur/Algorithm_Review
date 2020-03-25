c2ecb8744f16074b1592991e7c8f91c63f0d7334
Cop-Tur <824919441@qq.com>
2020-03-25 20:49:21 +0800

    二叉查找树

--- src/chapter_3/binarytree/BinarySearchTree.java
+++ src/chapter_3/binarytree/BinarySearchTree.java
@@ -1,5 +1,26 @@
 package chapter_3.binarytree;
 
 public class BinarySearchTree<AnyType extends Comparable<? super AnyType>> {
+    private static class BinaryNode<AnyType>{
+        BinaryNode(AnyType theElement){
+            this(theElement,null,null);
+        }
+        BinaryNode(AnyType theElement,BinaryNode<AnyType> lt,BinaryNode<AnyType> rt){
+            element=theElement;
+            left=lt;
+            right=rt;
+        }
+        AnyType element;
+        BinaryNode<AnyType> left;
+        BinaryNode<AnyType> right;
+    }
 
+    private BinaryNode<AnyType> root;
+
+    public BinarySearchTree(){
+        root=null;
+    }
+    public void makeEmpty(){
+        root=null;
+    }
 }
