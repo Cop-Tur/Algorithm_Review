f5f8c9e20a7ae60097f0a85519e1412b28cc461b
Cop-Tur <824919441@qq.com>
2020-03-26 23:54:15 +0800

    二叉查找树 insert remove实现

--- src/chapter_3/binarytree/BinarySearchTree.java
+++ src/chapter_3/binarytree/BinarySearchTree.java
@@ -2,6 +2,8 @@ package chapter_3.binarytree;
 
 import org.omg.CORBA.Any;
 
+import java.nio.BufferUnderflowException;
+
 public class BinarySearchTree<AnyType extends Comparable<? super AnyType>> {
     private static class BinaryNode<AnyType>{
         BinaryNode(AnyType theElement){
@@ -25,6 +27,7 @@ public class BinarySearchTree<AnyType extends Comparable<? super AnyType>> {
     public void makeEmpty(){
         root=null;
     }
+
     public boolean isEmpty(){
         return root == null;
     }
@@ -32,6 +35,17 @@ public class BinarySearchTree<AnyType extends Comparable<? super AnyType>> {
     public boolean contains(AnyType x){
         return contains(x,root);
     }
+
+    public AnyType findMin(){
+        if (isEmpty()) throw new UnderflowException();
+        return findMin(root).element;
+    }
+
+    public AnyType findMax(){
+        if (isEmpty()) throw new BufferUnderflowException();
+        return findMax(root).element;
+    }
+
 
 
     private boolean contains(AnyType x, BinaryNode<AnyType> t){
@@ -56,6 +70,37 @@ public class BinarySearchTree<AnyType extends Comparable<? super AnyType>> {
         if (t != null)
             while (t.right!=null)
                 t=t.right;
+        return t;
+    }
+
+    private BinaryNode<AnyType> insert(AnyType x,BinaryNode<AnyType> t){
+        if (t == null)
+            return new BinaryNode<>(x,null,null);
+        int compareResult = x.compareTo(t.element);
+
+        if (compareResult<0)
+            t.left=insert(x,t.left);
+        else if (compareResult>0)
+            t.right=insert(x,t.right);
+        else
+            ;
+        return t;
+    }
+    private BinaryNode<AnyType> remove(AnyType x,BinaryNode<AnyType> t){
+        if (t==null)
+            return t; //item not found; do nothig
+        int compareResult=x.compareTo(t.element);
+
+        if (compareResult<0)
+            t.left=remove(x,t.left);
+        else if (compareResult>0)
+            t.right=remove(x,t.right);
+        else if (t.left!=null && t.right!=null){
+            t.element=findMin(t.right).element;
+            t.right=remove(t.element,t.right);
+        }
+        else
+            t=(t.left != null)?t.left : t.right;
         return t;
     }
 
