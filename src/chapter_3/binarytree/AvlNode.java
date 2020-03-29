648ccb1b41377d7b87c027ee221ef9e03fee28ed
Cop-Tur <824919441@qq.com>
2020-03-29 19:59:02 +0800

    AVL树节点声明

--- src/chapter_3/binarytree/AvlNode.java
+++ src/chapter_3/binarytree/AvlNode.java
@@ -1,4 +1,20 @@
 package chapter_3.binarytree;
 
-public class AvlNode {
+public class AvlNode<AnyType> {
+
+    AnyType element;
+    AvlNode<AnyType> left;
+    AvlNode<AnyType> right;
+    int height;
+
+    AvlNode(AnyType theElement) {
+        this(theElement, null, null);
+    }
+
+    AvlNode(AnyType theElement, AvlNode<AnyType> lt, AvlNode<AnyType> rt) {
+        element = theElement;
+        left = lt;
+        right = rt;
+        height = 0;
+    }
 }
