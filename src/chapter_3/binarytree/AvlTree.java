48da7afa287562a6b0a725d6341bdcbde1c054ec
Cop-Tur <824919441@qq.com>
2020-04-02 22:53:20 +0800

    AVL树 单旋转

--- src/chapter_3/binarytree/AvlTree.java
+++ src/chapter_3/binarytree/AvlTree.java
@@ -57,4 +57,14 @@ public class AvlTree<AnyType extends Comparable<? super AnyType>> {
         t.height=Math.max(height(t.left),height(t.right))+1;
         return t;
     }
+
+    private AvlNode<AnyType> rotateWithLeftChild(AvlNode<AnyType> k2){
+        AvlNode<AnyType> k1=k2.left;
+        k2.left=k1.right;
+        k1.right=k2;
+        k2.height=Math.max(height(k2.left),height(k2.right))+1;
+        k1.height=Math.max(height(k1.left),k2.height)+1;
+        return k1;
+    }
+
 }
