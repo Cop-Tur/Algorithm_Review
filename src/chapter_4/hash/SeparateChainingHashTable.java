4e46515c10aa65f42bc01b5569b12fe27ec25fb8
Cop-Tur <824919441@qq.com>
2020-04-05 23:31:32 +0800

    分离链接散列表

--- src/chapter_4/hash/SeparateChainingHashTable.java
+++ src/chapter_4/hash/SeparateChainingHashTable.java
@@ -1,4 +1,21 @@
 package chapter_4.hash;
 
-public class SeparateChainingHashTable {
+import com.sun.xml.internal.bind.AnyTypeAdapter;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class SeparateChainingHashTable<AnyType> {
+    public SeparateChainingHashTable(){
+        this(DEFAULT_TABLE_SIZE);
+    }
+
+    public SeparateChainingHashTable(int size){
+        theLists = new LinkedList[nextPrime(size)]
+    }
+
+    private static final int DEFAULT_TABLE_SIZE = 101;
+
+    private List<AnyType>[] theLists;
+    private int currentSize;
 }
