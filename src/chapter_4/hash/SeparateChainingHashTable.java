161804af275c3a728ce97f8c06b56bfe1f496912
Cop-Tur <824919441@qq.com>
2020-04-08 23:44:32 +0800

    分离链接法

--- src/chapter_4/hash/SeparateChainingHashTable.java
+++ src/chapter_4/hash/SeparateChainingHashTable.java
@@ -22,8 +22,26 @@ public class SeparateChainingHashTable<AnyType> {
         currentSize = 0;
     }
 
+    public void insert(AnyType x){
+        List<AnyType> whichList = theLists[myhash(x)];
+        if (!whichList.contains(x)){
+            whichList.add(x);
+
+            if (++currentSize > theLists.length)
+                rehash();
+        }
+    }
+
     private static final int DEFAULT_TABLE_SIZE = 101;
 
     private List<AnyType>[] theLists;
     private int currentSize;
+
+    private int myhash(AnyType x){
+        int hashVal = x.hashCode();
+        hashVal %=theLists.length;
+        if (hashVal < 0)
+            hashVal += theLists.length;
+        return hashVal;
+    }
 }
