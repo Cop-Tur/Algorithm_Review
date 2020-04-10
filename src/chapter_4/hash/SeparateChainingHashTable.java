ed1b8ba86d1b5a78bc2a19a8fc1b9e803842cad9
Cop-Tur <824919441@qq.com>
2020-04-10 22:08:14 +0800

    分离链接法

--- src/chapter_4/hash/SeparateChainingHashTable.java
+++ src/chapter_4/hash/SeparateChainingHashTable.java
@@ -6,25 +6,25 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class SeparateChainingHashTable<AnyType> {
-    public SeparateChainingHashTable(){
+    public SeparateChainingHashTable() {
         this(DEFAULT_TABLE_SIZE);
     }
 
-    public SeparateChainingHashTable(int size){
+    public SeparateChainingHashTable(int size) {
         theLists = new LinkedList[nextPrime(size)];
-        for (int i=0;i<theLists.length;i++)
-            theLists[i]=new LinkedList<>();
+        for (int i = 0; i < theLists.length; i++)
+            theLists[i] = new LinkedList<>();
     }
 
-    public void makeEmpty(){
-        for (int i=0; i<theLists.length;i++)
+    public void makeEmpty() {
+        for (int i = 0; i < theLists.length; i++)
             theLists[i].clear();
         currentSize = 0;
     }
 
-    public void insert(AnyType x){
+    public void insert(AnyType x) {
         List<AnyType> whichList = theLists[myhash(x)];
-        if (!whichList.contains(x)){
+        if (!whichList.contains(x)) {
             whichList.add(x);
 
             if (++currentSize > theLists.length)
@@ -32,9 +32,9 @@ public class SeparateChainingHashTable<AnyType> {
         }
     }
 
-    public void remove(AnyType x){
+    public void remove(AnyType x) {
         List<AnyType> whichList = theLists[myhash(x)];
-        if (whichList.contains(x)){
+        if (whichList.contains(x)) {
             whichList.remove(x);
             currentSize++;
         }
@@ -45,9 +45,9 @@ public class SeparateChainingHashTable<AnyType> {
     private List<AnyType>[] theLists;
     private int currentSize;
 
-    private int myhash(AnyType x){
+    private int myhash(AnyType x) {
         int hashVal = x.hashCode();
-        hashVal %=theLists.length;
+        hashVal %= theLists.length;
         if (hashVal < 0)
             hashVal += theLists.length;
         return hashVal;
