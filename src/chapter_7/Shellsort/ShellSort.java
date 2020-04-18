6dd453ddd3cb85555b7abc8b3554b52c8b7fa9f7
Cop-Tur <824919441@qq.com>
2020-04-18 16:04:10 +0800

    shell sort

---
+++ src/chapter_7/Shellsort/ShellSort.java
@@ -0,0 +1,15 @@
+package chapter_7.Shellsort;
+
+public class ShellSort {
+    public static <AnyType extends Comparable<? super AnyType>>
+    void shellsort(AnyType [] a){
+        int j;
+        for (int gap=a.length/2;gap>0;gap/=2)
+            for (int i = gap;i<a.length;i++){
+                AnyType tmp = a[i];
+                for (j=i;j>=gap && tmp.compareTo(a[j-gap])<0;j-=gap)
+                    a[j] = a[j-gap];
+                a[j] = tmp;
+            }
+    }
+}
