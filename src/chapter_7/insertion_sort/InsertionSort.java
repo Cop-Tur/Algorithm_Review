f939a5dcda65ab0cbba667a279c951f63f06f023
Cop-Tur <824919441@qq.com>
2020-04-23 11:28:11 +0800

    insertion sort

--- src/chapter_7/insertion_sort/InsertionSort.java
+++ src/chapter_7/insertion_sort/InsertionSort.java
@@ -1,12 +1,25 @@
 package chapter_7.insertion_sort;
 
 public class InsertionSort {
-    public static <AnyType extends Comparable<? super AnyType>> void insertionSort(AnyType [] a){
+    public static <AnyType extends Comparable<? super AnyType>>
+    void insertionSort(AnyType[] a) {
         int j;
-        for (int p = 1;p < a.length; p++){
+        for (int p = 1; p < a.length; p++) {
             AnyType tmp = a[p];
-            for ( j = p; j > 0 && tmp.compareTo(a[ j - 1]) < 0; j--)
-                a[j] = a[j-1];
+            for (j = p; j > 0 && tmp.compareTo(a[j - 1]) < 0; j--)
+                a[j] = a[j - 1];
+            a[j] = tmp;
+        }
+    }
+
+    public static <AnyType extends Comparable<? super AnyType>>
+    void insertionSort(AnyType[] a, int left, int right) {
+        for (int p = left + 1; p <= right; p++) {
+            AnyType tmp = a[p];
+            int j;
+
+            for (j = p; j > left && tmp.compareTo(a[j - 1]) < 0; j--)
+                a[j] = a[j - 1];
             a[j] = tmp;
         }
     }
