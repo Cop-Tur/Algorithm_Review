8b34f1efa0ec67fd24c6539ed350b71afe8553c8
Cop-Tur <824919441@qq.com>
2020-04-28 21:38:52 +0800

    Tiny语言词法分析器

--- src/compiler_principle/Lexer.java
+++ src/compiler_principle/Lexer.java
@@ -1,12 +1,34 @@
 package compiler_principle;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class Lexer {
+    private static class Tuple {
+        public int row;
+        public int token;
+        public String statement;
+
+        public Tuple(int row, int token, String statement) {
+            this.row = row;
+            this.token = token;
+            this.statement = statement;
+        }
+    }
+
+    private static class Error{
+        public int row;
+        public String statement;
+
+        public Error(int row, String statement) {
+            this.row = row;
+            this.statement = statement;
+        }
+    }
+
     private ArrayList<String> inp = new ArrayList<>();
-    private HashMap<Integer, String> err = new HashMap<>();
+    private ArrayList<Error> err = new ArrayList<>();
+    private ArrayList<Tuple> tuples = new ArrayList<>();
 
     public void inpProgram() {
         Scanner sc = new Scanner(System.in);
@@ -18,14 +40,27 @@ public class Lexer {
                 int index1 = s.indexOf("{");
                 if (s.contains("}")) {
                     int index2 = s.indexOf("}");
-                    String tmp = s.replace(s.substring(index1, index2 + 2), "");
+                    String tmp = s.replace(s.substring(index1, index2 + 1), "");
                     inp.add(tmp);
                 } else {
-                    err.put(inp.size(), "'}'expected");
+                    err.add(new Error(inp.size(),"'}'expected"));
                 }
             }
             s = sc.nextLine();
         }
+    }
+
+    public void printTuple(Tuple tuple) {
+        if (tuple.token >= 1 && tuple.token <= 8)
+            System.out.println("   " + tuple.row + ": reserved word: " + tuple.statement);
+        if (tuple.token >= 9 && tuple.token <= 18)
+            System.out.println("   " + tuple.row + ": " + tuple.statement);
+        if (tuple.token == 19)
+            System.out.println("   " + tuple.row + ": NUM, value = " + tuple.statement);
+        if (tuple.token == 20)
+            System.out.println("   " + tuple.row + ": ID, name = " + tuple.statement);
+        if (tuple.token == 21)
+            System.out.println("   " + tuple.row + ": CHAR, value = \"" + tuple.statement + "\"");
     }
 
     public <AnyType> boolean contains(AnyType[] s, AnyType inp) {
@@ -40,8 +75,8 @@ public class Lexer {
         return s.matches("[0-9]*");
     }
 
-    public boolean isLetter(char s) {
-        return Character.isLetter(s);
+    public boolean isLetter(char ch) {
+        return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z');
     }
 
     public int getKeyToken(String s) {
@@ -62,9 +97,10 @@ public class Lexer {
         return token;
     }
 
-    /*public void lexAnalys() {
+    public void lexAnalys() {
         for (int row = 0; row < inp.size(); row++) {
             String line = inp.get(row);
+            System.out.println(row + ": " + line);
             int i = 0;
             char ch;
 
@@ -72,15 +108,13 @@ public class Lexer {
                 ch = line.charAt(i);
                 //判断读取的第一个字符是否为字母
                 if (isLetter(ch)) {
-                    StringBuffer stringBuffer = new StringBuffer();
-                    stringBuffer.append(ch);
-
+                    StringBuilder stringBuilder = new StringBuilder();
+                    stringBuilder.append(ch);
                     //读取下一个字符
                     ch = line.charAt(++i);
-                    //非第一个字符时，可以为字母和数字
+                    //第一个字母，循环读取后面的字母或数字
                     while ((isLetter(ch)) || isNumber(ch + "")) {
-                        stringBuffer.append(ch);
-                        //如果读取到行的末尾则跳出循环，否则继续读取
+                        stringBuilder.append(ch);
                         if (i == line.length() - 1) {
                             i++;
                             break;
@@ -89,151 +123,118 @@ public class Lexer {
                         }
                     }
 
-                    //判断当前字符串是否是关键字
-                    if (contains(KEY, stringBuffer.toString())) {
-                        //获取该关键字的种别码
-                        int kindCode = getKeyToken(stringBuffer.toString());
-                        //输出该关键字的二元组
-                        System.out.print("(" + kindCode + "," + stringBuffer.toString() + ")" + "  ");
+                    //判断当前字符串是否是key
+                    if (contains(KEY, stringBuilder.toString())) {
+                        //获取该key的token
+                        int token = getKeyToken(stringBuilder.toString());
+                        //输出二元组
+                        tuples.add(new Tuple(row, token, stringBuilder.toString()));
+                        printTuple(new Tuple(row, token, stringBuilder.toString()));
                     }
                     //标识符，输出标识符的二元组
                     else {
-                        System.out.print("(" + "变量" + "," + stringBuffer.toString() + ")" + "  ");
+                        tuples.add(new Tuple(row, 20, stringBuilder.toString()));
+                        printTuple(new Tuple(row, 20, stringBuilder.toString()));
                     }
                 }
                 //如果是symbol
-                else if (contains(SYMBOL, ch)) {
-
-                    System.out.println("(" + getSymbolToken(ch + "") + "," + ch + ")" + "  ");
+                else if (contains(SYMBOL, ch + "")) {
+                    tuples.add(new Tuple(row, getSymbolToken(ch + ""), ch + ""));
+                    printTuple(new Tuple(row, getSymbolToken(ch + ""), ch + ""));
                     i++;
                 }
-                //如果是':',需要继续读取一个字符进行判断是否是双界符
+                //如果是':',需要继续读取一个字符进行判断是否是:=
                 else if (ch == ':') {
-                    StringBuffer stringBuffer = new StringBuffer();
-                    stringBuffer.append(ch);
-                    //读取下一个字符
+                    StringBuilder stringBuilder = new StringBuilder();
+                    stringBuilder.append(ch);
                     char nextCh = line.charAt(++i);
                     //如果字符为'='
                     if (nextCh == '=') {
-                        stringBuffer.append(nextCh);
-                        //输出双界符相关的二元数组
-                        System.out.print("(" + getSymbolToken(stringBuffer.toString())
-                                + "," + stringBuffer.toString() + ")" + "  ");
+                        stringBuilder.append(nextCh);
+                        tuples.add(new Tuple(row, getSymbolToken(stringBuilder.toString()), stringBuilder.toString()));
+                        printTuple(new Tuple(row, getSymbolToken(stringBuilder.toString()), stringBuilder.toString()));
                         i++;
                     }
-                    //单单一个冒号 错误
+                    //只有一个冒号 错误
                     else {
-                        err.put(row, "'='expected");
+                        err.add(new Error(row, "'='expected"));
                     }
                 }
 
-
-                //如果读取的字符为'或"
-                else if ((ch + "").equals("'") || ch == '"') {
-                    StringBuffer stringBuffer = new StringBuffer();
-                    stringBuffer.append(ch);
+                //如果读取的字符为"
+                else if (ch == '"') {
+                    StringBuilder stringBuilder = new StringBuilder();
+                    stringBuilder.append(ch);
                     if (i == line.length() - 1) {
                         i++;
                         break;
-                    }
-                    //继续读取字符
-                    else {
+                    } else {
                         ch = line.charAt(++i);
+                        while (ch != '"') {
+                            stringBuilder.append(ch);
+                            if (i == line.length() - 1) {
+                                i++;
+                                break;
+                            } else {
+                                ch = line.charAt(++i);
+                            }
+                        }
                     }
-
-                    if (stringBuffer.charAt(0) == dyh.charAt(0)) {
-                        StringBuffer stringBuffer1 = new StringBuffer();
-                        stringBuffer1.append(ch);
-                        col++;
+                    if (ch == '"') {
+                        //输出的是字符常数
+                        tuples.add(new Tuple(row, 21, stringBuilder.toString()));
+                        printTuple(new Tuple(row, 21, stringBuilder.toString()));
+                    } else {
+                        err.add(new Error(row, "'\"'expected"));
+                    }
+                    i++;
+                }
+                //非法字符
+                else if (!contains(SYMBOL, ch) && !isNumber(ch + "") && !isLetter(ch) && ch != ' ' && ch != '\t') {
+                    err.add(new Error(row, "error character!"));
+                    i++;
+                }
+                //第一次读入的是数字
+                else if (isNumber(ch + "")) {
+                    StringBuilder stringBuilder = new StringBuilder();
+                    while (isNumber(ch + "") || ch == '.') {
+                        stringBuilder.append(ch);
                         if (i == line.length() - 1) {
                             i++;
                             break;
-                        } else {
-                            ch = line.charAt(++i);
-                            col++;
-                            while (ch != dyh.charAt(0)) {
-                                stringBuffer.append(ch);
-                                if (i == line.length() - 1) {
-                                    i++;
-                                    break;
-                                } else {
-                                    //继续读取字符
-                                    ch = line.charAt(++i);
-                                    col++;
-                                }
-                            }
                         }
-                        if (ch == dyh.charAt(0)) {
-                            //输出的是字符常数
-                            System.out.print("(" + 36 + "," + stringBuffer1.toString() + ")" + "  ");
-                            count++;
-                        } else {
-                            System.out.print("error:单引号不匹配" + "第" + row + "行,第" + col + "列");
-                        }
-                        i++;
-                    }
-                }
-                if (count % 5 == 0) {
-                    System.out.println();
-                }
-            }
-            //非法字符判断
-                else if (isDlimeter(ch) == false && isNumber(ch) == false && isLetter(ch) == false && ch != ' ') {
-                System.out.print("error:出现非法字符：" + ch + ",第" + row + "行,第" + col + "列");
-                i++;
-                col++;
-            }
-            //第一次读入的是数字
-            else if (isNumber(ch)) {
-                StringBuffer stringBuffer = new StringBuffer();
-                stringBuffer.append(ch);
-                col++;
-                ch = line.charAt(++i);
-                if (isNumber(ch)) {
-                    while (isNumber(ch)) {
-                        stringBuffer.append(ch);
-                        col++;
                         ch = line.charAt(++i);
                     }
-                    System.out.print("(" + 35 + "," + stringBuffer.toString() + ")" + "");
-                    count++;
+                    tuples.add(new Tuple(row, 19, stringBuilder.toString()));
+                    printTuple(new Tuple(row, 19, stringBuilder.toString()));
+
+                } else {
+                    i++;
                 }
-                //一位数时，输出二元组
-                else {
-                    System.out.print("(" + 35 + "," + stringBuffer.toString() + ")" + " ");
-                    count++;
-                }
-                if (isLetter(ch)) {
-                    while (isLetter(ch)) {
-                        stringBuffer.append(ch);
-                        col++;
-                        ch = line.charAt(++i);
-                    }
-                    System.out.print("error:非法字符" + stringBuffer.toString() + " 第 " + row + " 行,第" + col + " 列出错");
-                }
-                if (count % 5 == 0) {
-                    System.out.println();
-                }
-            } else {
-                i++;
-                col++;
             }
         }
+        for (Error error : err) {
+            System.out.println("error: " + error.statement + " at row: " + error.row);
+
+        }
+    }
 
 
-    }*/
-
-
-    private static final String[] KEY = {"if", "then", "else", "end", "repeat",
+    private static final String[] KEY = {"if", "then", "else", "endif", "repeat",
             "until", "read", "write"};
     private static final String[] SYMBOL = {"+", "-", "*", "/", "=", "<", "(", ")", ";", ":="};
+
+    /*
+    if 1 then 2 else 3 end 4 repeat 5 until 6 read 7 write 8
+    + 9  - 10  * 11  / 12  = 13  < 14  ( 15  ) 16  ; 17  := 18
+    数 19  标识符 20  字符 21
+
+    */
+
 
     public static void main(String[] args) {
         Lexer lexer = new Lexer();
         lexer.inpProgram();
-        for (String s : lexer.inp
-        ) {
-            System.out.println(s);
-        }
+        lexer.lexAnalys();
     }
 }
